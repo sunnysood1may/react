1d2f89dcf5adeea7806f83dea278ba6bd440e837
sunny sood <sunny1may@gmail.com>
2024-12-06 19:45:20 +0530

    uncontrolledComponent

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useRef } from "react";
 
 function App() {
-  const [val, setVal] = useState("1000");
+  let inputRef = useRef(null);
+  let inputRef2 = useRef(null);
+  function submitForm(e) {
+    e.preventDefault();
+    console.log("input field 1 value : ", inputRef.current.value)
+    console.log("input field 2 value : ", inputRef2.current.value)
+    let input3 = document.getElementById("input3").value;
+    console.log("input field 3 value : ", input3)
+  }
   return (
     <div className="App">
-      <h1>Controlled Component </h1>
-      <input
-        type="text"
-        value={val}
-        onChange={(e) => setVal(e.target.value)}
-      />{" "}
-      <br />
-      <h2>Value: {val}</h2>
+      <h1>Uncontrolled Component </h1>
+      <form onSubmit={submitForm}>
+        <input ref={inputRef} type="text" /> <br /> <br />
+        <input ref={inputRef2} type="text" /> <br /> <br />
+        <input id="input3" type="text" /> <br /> <br />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
