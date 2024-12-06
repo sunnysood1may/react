33ac2c0de089ebe880a1602360fb158721e56539
sunny sood <sunny1may@gmail.com>
2024-12-06 17:34:22 +0530

    use memo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React, { useEffect, useState, Fragment } from "react";
+import React, { useEffect, useState, Fragment, useMemo } from "react";
 import User from "./User";
 import Student from "./Student";
 import "./style.css";
@@ -9,22 +9,23 @@ import { Button, Alert, Table } from "react-bootstrap";
 import Cols from "./Col";
 import Counter from "./Counter";
 
-class App extends React.Component {
-  constructor(){
-    super();
-    this.state={
-      count:1
-    }
-  }
-  render(){
-    return (
-      <div className="App">
-        <Counter count={this.state.count} />
-       <button onClick={()=>{this.setState({count:this.state.count+1})}}>Update count</button>
-      </div>
-    );
-  }
+function App() {
+  const [count, setCount] = useState(0);
+  const [item, setItem] = useState(10);
+  const newApple=React.useMemo(
+    function appleTime(){
+      return count*5;
+    },[count]
+  )
   
+  return (
+    <div className="App">
+      <h1>Hooks in React {count}</h1>
+      <h1>{newApple}</h1>
+      <button onClick={() => setCount(count + 1)}>Update Count</button>
+      <button onClick={() => setItem(item * 10)}>Update Item</button>
+    </div>
+  );
 }
 
 export default App;
