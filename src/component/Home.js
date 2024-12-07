2f769d2ebf82159ff72189fa636413a66ab639ea
sunny sood <sunny1may@gmail.com>
2024-12-07 08:14:07 +0530

    Navigation on Click

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,12 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+  const navToPage = (url) => {
+    navigate(url);
+  };
   return (
     <>
       <h1>Home page</h1>
       <p>This is a Home Page of our App</p>
       <p>We are learning about router</p>
       <Link to="/about">Go to about</Link>
+      <br />
+      <button onClick={() => navigate("/about")}>navigate to about</button>
+      <br />
+      <br />
+      <button onClick={() => navigate("/filter")}>navigate to filter</button>
+      <br />
+      <br />
+
+      <button onClick={() => navToPage("/about")}>About</button>
+      <br />
+      <br />
+      <button onClick={() => navToPage("/filter")}>Filter</button>
     </>
   );
 }
