eabe058af19dc23f790275d110bdd055bfe3ac62
sunny sood <sunny1may@gmail.com>
2024-12-07 22:32:08 +0530

    update data

--- src/crud/listing.js
+++ src/crud/listing.js
@@ -2,7 +2,12 @@ import React, { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 function Listing() {
-  const [data, setData] = useState([]);
+  const [users, setUser] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [userId, setUserId] = useState(null);
+
   useEffect(() => {
     getUsers();  
   }, []);
@@ -10,8 +15,12 @@ function Listing() {
   function getUsers(){
     fetch("https://jsonplaceholder.typicode.com/users").then((result) => {
         result.json().then((resp) => {
-          console.log("result", resp);
-          setData(resp);
+          //console.log("result", resp);
+          setUser(resp);
+          setName(resp[0].name);
+          setEmail(resp[0].email);
+          setPhone(resp[0].phone);
+          setUserId(resp[0].id);
         });
     });
   }
@@ -21,36 +30,73 @@ function Listing() {
       method: 'DELETE'
     }).then((result) => {
       result.json().then((resp) => {
-        console.warn(resp);
+        //console.warn(resp);
         getUsers();
       })
     })
+  }
+
+  function selectUser(id) {
+    let item  = users[id-1]; console.log(item);
+    setName(item.name);
+    setEmail(item.email);
+    setPhone(item.phone);
+    setUserId(item.id);
+  }
+
+  function updateUser() {
+    let item = {name,email,phone,userId};
+    console.log(item);
+    fetch(`https://jsonplaceholder.typicode.com/todo/${userId}`, {
+        method: 'PUT',
+        headers:{
+        'Accept':'application/json',
+        'Content-Type':'application/json'
+        },
+        body:JSON.stringify(item)
+      }).then((result) => {
+        result.json().then((resp) => {
+          console.warn(resp)
+          getUsers()
+        })
+      })
+
   }
 
   return (
     <div>
       <h2>Users Listing</h2> 
       <Link className="nav-bar-link" to="/users/add">Add User</Link>
-      <table border="1" align="center">
+      <table border="1" align="left">
         <tbody>
           <tr>
             <td>ID</td>
             <td>Name</td>
             <td>Email</td>
             <td>Phone</td>
-            <td>Delete</td>
+            <td>Operations</td>
           </tr>
-          {data.map((item, i) => (
+          {users.map((item, i) => (
             <tr key={i}>
               <td>{item.id}</td>
               <td>{item.name}</td>
               <td>{item.email}</td>
               <td>{item.phone}</td>
               <td><button onClick={() => deleteUser(item.id)}>delete</button></td>
+              <td><button onClick={() => selectUser(item.id)}>Update</button></td>
             </tr>
           ))}
         </tbody>
       </table>
+
+      <div>
+        <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}} /><br /><br />
+        <input type="text" value={email} onChange={(e)=>{setEmail(e.target.value)}} /><br /><br />
+        <input type="text" value={phone} onChange={(e)=>{setPhone(e.target.value)}} /><br /><br />
+        <button onClick={updateUser}>Update user</button>
+        </div>
+
+
     </div>
   );
 }
