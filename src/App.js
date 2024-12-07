1e76d23babfd600f7fda4410db39321bdfc56adc
sunny sood <sunny1may@gmail.com>
2024-12-07 15:31:35 +0530

    404 not found

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import './App.css';
-import React from 'react'
-import { Link, Route, Routes } from 'react-router-dom'
-import Nav from './component/Nav';
-import Home from './component/Home';
-import About from './component/About';
+import "./App.css";
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import Nav from "./component/Nav";
+import Home from "./component/Home";
+import About from "./component/About";
+import Page404 from "./component/Page404";
 function App() {
   return (
     <div className="App">
-        <Nav />
-        <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route exact path="/about" element={<About/>} />
-        </Routes> 
+      <Nav />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route path="/*" element={<Page404 />} />
+      </Routes>
     </div>
   );
 }
