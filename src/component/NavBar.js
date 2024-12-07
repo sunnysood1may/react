ca1523113700ffc8706a81a8f02b325665dd0dc5
sunny sood <sunny1may@gmail.com>
2024-12-07 07:08:27 +0530

    style on navlink

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,23 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
     <div>
-      <ul>
-        <li>
-          <Link to="/about">About</Link>
+      <ul className="navbar">
+      <li>
+          <NavLink className="nav-bar-link" style={{color:'#fff',backgroundColor:'grey'}} to="/">Home</NavLink>
         </li>
         <li>
-          <Link to="/">Home</Link>
+          <Link className="nav-bar-link" to="/about">About</Link>
+        </li>    
+        <li>
+          <Link className="nav-bar-link" to="/contact">Contact</Link>
         </li>
         <li>
-          <Link to="/contact">Contact</Link>
+          <Link className="nav-bar-link" to="/user/sunny">sunny</Link>
         </li>
         <li>
-          <Link to="/user/sunny">sunny</Link>
-        </li>
-        <li>
-          <Link to="/user/shilpa">shilpa</Link>
+          <Link className="nav-bar-link" to="/user/shilpa">shilpa</Link>
         </li>
 
         <li>
